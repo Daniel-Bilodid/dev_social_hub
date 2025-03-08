11ba6a5f11f1b2d6a22029c9a5e82957b0b4cd71
Daniel <danielbeloded@gmail.com>
2025-03-08 15:59:03 +0200

    uploading new project

---
+++ src/app/nav/page.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+import SideMenu from "../side-menu/page";
+import { CgProfile } from "react-icons/cg";
+import { FiSearch } from "react-icons/fi";
+
+const Nav = () => {
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="bg-gray-900 dark:bg-gray-800 p-6 shadow-md flex items-center justify-between">
+        <div className="text-2xl font-bold text-white">DevHub</div>
+
+        <div className="relative w-full max-w-[600px]">
+          <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full px-10 py-2 rounded-md border border-gray-600 dark:bg-gray-700 dark:text-white placeholder-gray-400"
+          />
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <Link
+            href="/profile"
+            className="text-white dark:text-white hover:text-indigo-500 w-[40px] h-[40px] bg-gray-600  flex items-center justify-center rounded-[5px]"
+          >
+            <CgProfile className="w-6 h-6" />
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex flex-1">
+        <div className="w-64 bg-gray-900 dark:bg-gray-800">
+          <SideMenu />
+        </div>
+
+        <div className="flex-1 bg-gray-100 dark:bg-gray-900 p-4">
+          <main></main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/app/side-menu/page.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+const SideMenu = () => {
+  return (
+    <div className="w-64 bg-gray-800 text-white p-4">
+      <ul className="space-y-4">
+        <li>
+          <Link href="/" className="hover:text-blue-400">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/questions" className="hover:text-blue-400">
+            Questions
+          </Link>
+        </li>
+        <li>
+          <Link href="/tags" className="hover:text-blue-400">
+            Tags
+          </Link>
+        </li>
+        <li>
+          <Link href="/users" className="hover:text-blue-400">
+            Users
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideMenu;
